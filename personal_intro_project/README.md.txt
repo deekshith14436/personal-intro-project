f1c2f9e9637e8993aba9dc0b1e3bedc7972f2e0f
deekshith14436 <158846215+deekshith14436@users.noreply.github.com>
2025-12-17 12:52:19 +0530

    Add files via upload

---
+++ personal_intro_project/README.md.txt
@@ -0,0 +1,8 @@
+# Personal Intro Program
+
+This Python program collects user details like name, age, and hobby.
+It then prints a greeting using f-strings.
+
+## How to Run
+
+click  run button 
---
+++ personal_intro_project/final code.py
@@ -0,0 +1,10 @@
+
+name = input("What is your name? ")
+age = input("How old are you? ")
+hobby = input("What is your favorite hobby? ")
+
+
+print()
+
+print(f"🎉 Welcome {name}! 🎉")
+print(f"You are {age} years old and love {hobby}.")
